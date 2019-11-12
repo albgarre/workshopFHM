0d41b1935ff8bac1da9df3fbe522cd09dbf90daa
Alberto <garre.alberto@gmail.com>
2019-11-12 15:15:30 +0100

    Data for lesson 2 (ggplot2)

---
+++ NEWS.md
@@ -0,0 +1,4 @@
+
+# Version 0.1.0
+
+* Included the datasets for lesson II (ggplot2).
---
+++ R/data.R
@@ -0,0 +1,62 @@
+#' Some inactivation experiments
+#'
+#' A dataset containing some inactivation experiments.
+#'
+#' @format A data frame with 75 rows and 5 variables:
+#' \describe{
+#'   \item{Date}{Date of the experiment}
+#'   \item{Experimenter}{Name of the experimenter}
+#'   \item{Timepoint}{Elapsed time}
+#'   \item{log_cfu}{Log-microbial count}
+#'   \item{Strain}{Microbial strain}
+#' }
+#'
+"Inactivation"
+
+#' A larger set of inactivation experiments
+#'
+#' A dataset containing more inactivation experiments.
+#'
+#' @format A data frame with 75000 rows and 5 variables:
+#' \describe{
+#'   \item{Date}{Date of the experiment}
+#'   \item{Experimenter}{Name of the experimenter}
+#'   \item{Timepoint}{Elapsed time}
+#'   \item{log_cfu}{Log-microbial count}
+#'   \item{Strain}{Microbial strain}
+#' }
+#'
+"Inactivation_75K"
+
+#' Dataset of inactivation experiments for the ggplot2 lesson
+#'
+#' A dataset containing some inactivation experiments.
+#'
+#' @format A data frame with 750 rows and 6 variables:
+#' \describe{
+#'   \item{Date}{Date of the experiment}
+#'   \item{Experimenter}{Name of the experimenter}
+#'   \item{Timepoint}{Elapsed time}
+#'   \item{log_cfu}{Log-microbial count}
+#'   \item{Strain}{Microbial strain}
+#'   \item{Supervisor}{Name of the tyrant}
+#' }
+#'
+"Inactivation_course"
+
+#' Filtered dataset of inactivation experiments for the ggplot2 lesson
+#'
+#' A dataset containing some inactivation experiments that has been filtered
+#' already.
+#'
+#' @format A data frame with 750 rows and 6 variables:
+#' \describe{
+#'   \item{Date}{Date of the experiment}
+#'   \item{Experimenter}{Name of the experimenter}
+#'   \item{Timepoint}{Elapsed time}
+#'   \item{log_cfu}{Log-microbial count}
+#'   \item{Strain}{Microbial strain}
+#'   \item{Supervisor}{Name of the tyrant}
+#' }
+#'
+"Inactivation_course_filtered"
